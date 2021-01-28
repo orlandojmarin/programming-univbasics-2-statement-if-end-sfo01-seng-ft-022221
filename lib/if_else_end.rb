d4d84ac410d7ff799fb928b17d4af1eced3e9768
Orlando Marin <orlandojermainemarin@gmail.com>
2021-01-28 16:59:58 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,3 @@
 # Write your solution here
-run_code_inside = false
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
+current_time = Time.now
+if time.now
